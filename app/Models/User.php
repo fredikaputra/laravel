0251bae95f39ac0e5485dc0e8e891f9388a4a61a
Fredika Putra <53744519+fredikaputra@users.noreply.github.com>
2025-05-31 05:00:15 +0000

    WIP

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\AsStringable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -26,7 +28,10 @@ class User extends Authenticatable implements OAuthenticatable
      *
      * @var list<string>
      */
-    protected $hidden = [];
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
     /**
      * Get the attributes that should be cast.
@@ -35,6 +40,10 @@ class User extends Authenticatable implements OAuthenticatable
      */
     protected function casts(): array
     {
-        return [];
+        return [
+            'name' => AsStringable::class,
+            'email' => AsStringable::class,
+            'email_verified_at' => 'datetime',
+        ];
     }
 }
